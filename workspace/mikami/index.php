99aa0c12f6ba190c18d77ea55ebb20d1d8d622e7
Chiho99 <chihomikami.81@gmail.com>
2020-07-03 14:19:32 +0900

    add link

--- workspace/mikami/index.php
+++ workspace/mikami/index.php
@@ -16,21 +16,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/035034ff28.js" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>PV ranking</title>
 </head>
 <body>
     <header>
         <div class="header_upper">
             <div class="header_upper_left">
                 <span><img src="../../img/prtimeslogo.png" alt="prtimes_logo"></span>
-                <span>プレートリリース・ニュースリリース配信サービスのPR TIMES</span>
+                <span>プレースリリース・ニュースリリース配信サービスのPR TIMES</span>
             </div>
             <div class="header_upper_right">
-                <!-- <ul>
-                    <li>プレリリースを配信</li>
-                    <li>配信を依頼</li>
-                    <li>ログイン</li>
-                </ul> -->
+                <ul class="header_upper_list">
+                    <li><a href="#">プレリリースを配信</a></li>
+                    <li><a href="#">配信を依頼</a></li>
+                    <li><a href="#">ログイン<i class="fas fa-search"></i></a></li>
+                </ul>
             </div>
         </div>
         <div class="header_bottom">
@@ -52,7 +52,7 @@
     </header>
     <main>
         <div class="main_left">
-            PVランキング一覧<hr>
+            <h2>PVランキング一覧</h2><hr>
             <ul class="pv_ranking">
                 <!-- ここにPVランキングデータを反映 -->
               <?php for($a = 0;$a < 30;$a++) { ?>
@@ -75,7 +75,7 @@
                 <li>
                     <img src="https://prtimes.jp/i/30268/259/resize/d30268-259-358451-5.jpg"><br>
                     <span>推しの香りがあなたのそばに⁉ 「ホロライブ」所属タレントをイメージした、100％天然精油配合のマスク用アロマスプレー『HOLO AROMA!』の発売決定！</span><br>
-                </li><hr> -->
+                </li><hr>
 
               <!-- もっと見るボタンをランキングリスト一番下のデータの次のリスト -->
                 <li class="load_more">
@@ -87,102 +87,103 @@
         <div class="main_right">
             <!-- 検索ボタン -->
             <i class="fas fa-search"></i>検索<br>
-            <input value="キーワードで検索"><button><i class="fas fa-search"></i></button>
+            <input value="キーワードで検索" class="search" placeholder="">
+            <button><i class="fas fa-search"></i></button><hr>
                 <!-- pv categoryのリスト -->
                 <div class="pv_category">
-                    <ol>
-                        <li>パソコン・周辺機器</li>
-                        <li>パソコンソフトウェア</li>
-                        <li>プロバイダ・回線接続</li>
-                        <li>ネットサービス</li>
-                        <li>スマートフォンアプリ</li>
-                        <li>サーバ・周辺機器</li>
-                        <li>ネットワーク・ネットワーク機器</li>
-                        <li>アプリケーション・セキュリティ</li>
-                        <li>システム・Webサイト・アプリ開発</li>
-                        <li>百貨店・スーパー・コンビニ・ストア</li>
-                        <li>EC・通販</li>
-                        <li>卸売・問屋</li>
-                        <li>芸能</li>
-                        <li>映画・演劇・DVD</li>
-                        <li>音楽</li>
-                        <li>テレビ・CM</li>
-                        <li>スポーツ</li>
-                        <li>アウトドア・登山</li>
-                        <li>雑誌・本・出版物</li>
-                        <li>漫画・アニメ</li>
-                        <li>アート・カルチャー</li>
-                        <li>コンシューマーゲーム</li>
-                        <li>スマートフォンゲーム</li>
-                        <li>アーケードゲーム</li>
-                        <li>おもちゃ・遊具・人形</li>
-                        <li>モバイル端末</li>
-                        <li>カメラ</li>
-                        <li>AV機器</li>
-                        <li>調理・生活家電</li>
-                        <li>健康・美容家電</li>
-                        <li>インテリア・家具・収納</li>
-                        <li>日用品・生活雑貨</li>
-                        <li>ガーデン・DIY</li>
-                        <li>自動車・カー用品</li>
-                        <li>バイク・バイク用品</li>
-                        <li>レディースファッション</li>
-                        <li>メンズファッション</li>
-                        <li>シューズ・バッグ</li>
-                        <li>ジュエリー・アクセサリー</li>
-                        <li>キッズ・ベビー・マタニティ</li>
-                        <li>食品・お菓子</li>
-                        <li>ソフトドリンク・アルコール飲料</li>
-                        <li>レストラン・ファストフード・居酒屋</li>
-                        <li>中食・宅配</li>
-                        <li>スキンケア・化粧品・ヘア用品</li>
-                        <li>ダイエット・健康食品・サプリメント</li>
-                        <li>医療・病院</li>
-                        <li>医薬・製薬</li>
-                        <li>福祉・介護・リハビリ</li>
-                        <li>経営・コンサルティング</li>
-                        <li>シンクタンク</li>
-                        <li>財務・経理</li>
-                        <li>法務。特許。知的財産</li>
-                        <li>銀行・信用金庫・信用組合</li>
-                        <li>クレジットカード・ローン</li>
-                        <li>証券・FX・投資信託</li>
-                        <li>生命保険・損害保険</li>
-                        <li>広告・宣伝・PR</li>
-                        <li>マーケティング・リサーチ</li>
-                        <li>セールス・営業</li>
-                        <li>就職・転職。人材派遣・アルバイト</li>
-                        <li>資格・留学・語学</li>
-                        <li>学校・大学</li>
-                        <li>学習塾・予備校・通信教育</li>
-                        <li>保育・幼児教育</li>
-                        <li>ホテル・旅館</li>
-                        <li>旅行・観光</li>
-                        <li>テーマパーク・遊園地</li>
-                        <li>住宅・マンション</li>
-                        <li>商業施設・オフィスビル</li>
-                        <li>建築・空間デザイン</li>
-                        <li>建設・土木</li>
-                        <li>鉄鋼・金属・ガラス・土石・ゴム</li>
-                        <li>化学</li>
-                        <li>電気・ガス・資源・エネルギー</li>
-                        <li>交通・運送・引越し</li>
-                        <li>物流・倉庫・貨物</li>
-                        <li>自然・天気</li>
-                        <li>環境・エコ・リサイクル</li>
-                        <li>ペット・ペット用品</li>
-                        <li>ギフト・花</li>
-                        <li>恋愛・結構</li>
-                        <li>出産・育児</li>
-                        <li>葬儀</li>
-                        <li>政治。官公庁・地方自治体</li>
-                        <li>財団法人・社団法人・宗教法人</li>
-                        <li>ボランティア</li>
-                        <li>国際情報・国際サービス</li>
-                        <li>農林・水産</li>
-                        <li>その他</li>
-                        <li>フィットネス・ヘルスケア</li>
-                        <li>電子部品・半導体・電気機器</li>
+                    <ol class="category_order">
+                        <li><a href="xpage1.php?cat=3&lim=1">パソコン・周辺機器</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=2">パソコンソフトウェア</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=3">プロバイダ・回線接続</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=4">ネットサービス</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=5">スマートフォンアプリ</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=6">サーバ・周辺機器</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=7">ネットワーク・ネットワーク機器</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=8">アプリケーション・セキュリティ</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=9">システム・Webサイト・アプリ開発</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=10">百貨店・スーパー・コンビニ・ストア</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=11">EC・通販</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=12">卸売・問屋</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=13">芸能</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=14">映画・演劇・DVD</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=15">音楽</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=16">テレビ・CM</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=17">スポーツ</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=18">アウトドア・登山</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=19">雑誌・本・出版物</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=20">漫画・アニメ</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=21">アート・カルチャー</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=22">コンシューマーゲーム</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=23">スマートフォンゲーム</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=24">アーケードゲーム</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=25">おもちゃ・遊具・人形</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=26">モバイル端末</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=27">カメラ</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=28">AV機器</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=29">調理・生活家電</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=30">健康・美容家電</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=31">インテリア・家具・収納</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=32">日用品・生活雑貨</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=33">ガーデン・DIY</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=34">自動車・カー用品</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=35">バイク・バイク用品</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=36">レディースファッション</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=37">メンズファッション</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=38">シューズ・バッグ</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=39">ジュエリー・アクセサリー</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=40">キッズ・ベビー・マタニティ</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=41">食品・お菓子</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=42">ソフトドリンク・アルコール飲料</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=43">レストラン・ファストフード・居酒屋</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=44">中食・宅配</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=45">スキンケア・化粧品・ヘア用品</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=46">ダイエット・健康食品・サプリメント</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=47">医療・病院</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=48">医薬・製薬</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=49">福祉・介護・リハビリ</li>
+                        <li><a href="xpage1.php?cat=3&lim=50">経営・コンサルティング</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=51">シンクタンク</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=52">財務・経理</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=53">法務。特許。知的財産</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=54">銀行・信用金庫・信用組合</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=55">クレジットカード・ローン</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=56">証券・FX・投資信託</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=57">生命保険・損害保険</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=58">広告・宣伝・PR</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=59">マーケティング・リサーチ</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=60">セールス・営業</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=61">就職・転職。人材派遣・アルバイト</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=62">資格・留学・語学<</a>/li>
+                        <li><a href="xpage1.php?cat=3&lim=63">学校・大学</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=64">学習塾・予備校・通信教育</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=65">保育・幼児教育</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=66">ホテル・旅館</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=67">旅行・観光</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=68">テーマパーク・遊園地</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=69">住宅・マンション</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=70">商業施設・オフィスビル</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=71">建築・空間デザイン</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=72">建設・土木</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=73">鉄鋼・金属・ガラス・土石・ゴム</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=74">化学</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=75">電気・ガス・資源・エネルギー</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=76">交通・運送・引越し</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=77">物流・倉庫・貨物</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=78">自然・天気</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=79">環境・エコ・リサイクル</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=80">ペット・ペット用品</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=81">ギフト・花</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=82">恋愛・結構</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=83">出産・育児</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=84">葬儀</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=85">政治。官公庁・地方自治体</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=86">財団法人・社団法人・宗教法人</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=87">ボランティア</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=88">国際情報・国際サービス</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=89">農林・水産</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=90">その他</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=91">フィットネス・ヘルスケア</a></li>
+                        <li><a href="xpage1.php?cat=3&lim=92">電子部品・半導体・電気機器</a></li>
                     </ol>
                 </div>
         </div>
@@ -191,35 +192,35 @@
     <footer>
         <div class="footer_upper">
             <ul class="footer_news">
-                <li><h2>ニュース配信サービス</h2></li>
-                <li>PR TIMESとは</li>
-                <li>料金・プラン</li>
-                <li>プレリリースを受信したい方へ</li>
-                <li>プレリリースを配信したい方へ</li>
-                <li>ものしりプロジェクト</li>
-                <li>ログイン</li>
+                <li><h2><a href="#">ニュース配信サービス</a></h2></li>
+                <li><a href="#">PR TIMESとは</a></li>
+                <li><a href="#">料金・プラン</a></li>
+                <li><a href="#">プレリリースを受信したい方へ</a></li>
+                <li><a href="#">プレリリースを配信したい方へ</a></li>
+                <li><a href="#">ものしりプロジェクト</a></li>
+                <li><a href="#">ログイン</a></li>
             </ul>
             <ul class="footer_recommend">
-                <li><h2>レコメンドサービス</h2></li>
-                <li>PR TIMES STORY</li>
-                <li>無料FAQならTayori</li>
-                <li>無料メールならTayori</li>
-                <li>タスク・プロジェクト管理ツールのJoto</li>
-                <li>PR TIMES TV</li>
-                <li>PR TIMES LIVE</li>
-                <li>PR TIMES MAGAZINE</li>
+                <li><h2><a href="#">レコメンドサービス</h2></a></li>
+                <li><a href="#">PR TIMES STORY</a></li>
+                <li><a href="#">無料FAQならTayori</a></li>
+                <li><a href="#">無料メールならTayori</a></li>
+                <li><a href="#">タスク・プロジェクト管理ツールのJoto</a></li>
+                <li><a href="#">PR TIMES TV</a></li>
+                <li><a href="#">PR TIMES LIVE</a></li>
+                <li><a href="#">PR TIMES MAGAZINE</a></li>
             </ul>
             <ul class="footer_recommend_wrap">
                     <li><h2><br></h2></li>
-                    <li>クリッピング</li>
-                    <li>広告ならAdGang</li>
-                    <li>無料占い・今週の運勢ならisuta</li>
-                    <li>無料恋愛占いisuta</li>
+                    <li><a href="#">クリッピング</a></li>
+                    <li><a href="#">広告ならAdGang</a></li>
+                    <li><a href="#">無料占い・今週の運勢ならisuta</a></li>
+                    <li><a href="#">無料恋愛占いisuta</a></li>
             </ul>
             <ul class="footer_sns">
-                <li><h2>PR TIMES公式SNS</h2></li>
-                <li>公式Facebookページ</li>
-                <li>Facebookカテゴリ
+                <li><h2><a href="#">PR TIMES公式SNS</h2></a></li>
+                <li><a href="#">公式Facebookページ</a></li>
+                <li><a href="#">Facebookカテゴリ</a>
                 <select name="" id="">
                     <!-- 実装するならモーダル -->
                     <!-- <option value="PR TIMES TV">PR TIMES TV</option>
@@ -238,8 +239,8 @@
                     <option value="ビジネス">ビジネス</option>
                     <option value="マーケティング">マーケティング</option> -->
                 </select></li><hr>
-                <li>公式Twitterページ</li>
-                <li>Twitterカテゴリ
+                <li><a href="#">公式Twitterページ</a></li>
+                <li><a href="#">Twitterカテゴリ</a>
                 <select name="" id="">
                     <!-- 実装するならモーダル -->
                     <!-- <option value="PR TIMES TV">PR TIMES TV</option>
@@ -263,16 +264,16 @@
         </div><hr>
         <div class="footer_bottom">
             <ul class="footer_bottom1">
-                <li>会社概要</li>
-                <li>プライバシーポリシー</li>
-                <li>情報セキュリティ</li>
-                <li>プレリリース掲載基準</li>
-                <li>利用規約</li>
+                <li><a href="#">会社概要</a></li>
+                <li><a href="#">プライバシーポリシー</a></li>
+                <li><a href="#">情報セキュリティ</a></li>
+                <li><a href="#">プレリリース掲載基準</a></li>
+                <li><a href="#">利用規約</a></li>
             </ul>
             <ul class="footer_bottom2">
-                <li>企業様のお問い合わせ</li>
-                <li>報道関係者様からのお問い合わせ</li>
-                <li>一般の皆様からのお問い合わせ</li>
+                <li><a href="#">企業様のお問い合わせ</a></li>
+                <li><a href="#">報道関係者様からのお問い合わせ</a></li>
+                <li><a href="#">一般の皆様からのお問い合わせ</a></li>
             </ul>
             <span>Copyright&copy; PR TIMES Inc. All Rights Reserved.</span>
         </div>
