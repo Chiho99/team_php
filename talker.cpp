318b2545c1fe4d83d440a2da00ff1da14324a2c0
emker <47836793+emker@users.noreply.github.com>
2021-10-10 20:16:54 +0900

    Add files via upload

---
+++ talker.cpp
@@ -0,0 +1,38 @@
+// %Tag(FULLTEXT)%
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include <sstream>
+
+/**
+ * This tutorial demonstrates simple sending of messages over the ROS system.
+ */
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle n;
+  ros::Publisher twistpub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+  
+  ros::Rate loop_rate(10);
+  int count = 0;
+  while (ros::ok())
+  {
+//    std_msgs::String msg;
+    geometry_msgs::Twist twist;
+//    std::stringstream ss;
+//    ss << "hello world " << count;
+//    msg.data = ss.str();
+//    ROS_INFO("%s", msg.data.c_str());
+    twist.linear.x=count*(count%2);
+    twist.angular.z=count*(1-count%2);
+    
+    twistpub.publish(twist);
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
